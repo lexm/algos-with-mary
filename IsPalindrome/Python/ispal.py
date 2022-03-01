a34762df648ef683c4fd4edb2ca095f054c396ec
Lex Myers <lex.myers@gmail.com>
2022-03-01 12:21:20 -0800

    ispalindrome function in python

--- IsPalindrome/Python/ispal.py
+++ IsPalindrome/Python/ispal.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
+from operator import truediv
 from os import lseek
 
+def ispalindrome(s):
+    for i in range((len(s) // 2)):
+        if s[i] != s[len(s) - i - 1]:
+            return False
+    return True
+
 f = open("../input.txt", "r")
-for str in f:
-    print(str)
+for s in f:
+    s = s.strip()
+    print(s + " " + str(ispalindrome(s)))
