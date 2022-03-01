69bc94c5fbdfa1ae6947a36606725f83ef491dac
Lex Myers <lex.myers@gmail.com>
2022-03-01 10:52:00 -0800

    .NET scaffold

---
+++ IsPalindrome/C#/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] lines = System.IO.File.ReadAllLines(@"../input.txt");
+foreach (string line in lines)
+{
+    Console.WriteLine(line);
+}
+
