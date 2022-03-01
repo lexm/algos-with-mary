5a62503a3b4023ab8d5918b62fc6ea2aa7d2fae8
Lex Myers <lex.myers@gmail.com>
2022-03-01 12:45:00 -0800

    IsPalindrome function added to .NET code

--- IsPalindrome/C#/Program.cs
+++ IsPalindrome/C#/Program.cs
@@ -1,7 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+bool IsPalindrome(string str) {
+    for(int i = 0; i < str.Length / 2; i++) {
+        if(str[i] != str[str.Length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 string[] lines = System.IO.File.ReadAllLines(@"../input.txt");
 foreach (string line in lines)
 {
-    Console.WriteLine(line);
+    Console.WriteLine($"{line}: {IsPalindrome(line)}");
 }
 
